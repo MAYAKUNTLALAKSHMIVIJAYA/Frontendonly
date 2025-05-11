461b36e74f01c82e3f38a661a7f16d4701437ba8
MAYAKUNTLALAKSHMIVIJAYA <mayakuntlalakshmi216@gmail.com>
2025-05-11 08:11:30 +0530

    Initial commit

---
+++ src/components/Logistics.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import logastic from "../images/cara1.jpg"
+
+export default function Logistics() {
+  return (
+    <div
+    style={{alignItems:'center',paddingTop:"200px",paddingLeft:'300px'}}><img src={logastic}/></div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,94 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore } from 'redux';
+import FarmerReducer from './redux/reducer/Farmerreducer';
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/Login';
+import Registration from './components/Registration';
+import Pagenotfound from './components/Pagenotfound';
+import AboutUs from './components/AboutUs';
+import Contactus from './components/Contactus';
+import FarmerLogin from './components/FarmerLogin';
+import ForgotPassword from './components/ForgotPassword';
+import Farmer from './components/Farmer';
+import Farmerdashboard from './components/Farmerdashboard';
+import AddProducts from './components/AddProducts';
+import TodaysRates from './components/TodaysRates';
+import Vendor from './components/Vendor';
+import VendorLogin from './components/VendorLogin';
+import Vendordashboard from './components/Vendordashboard';
+import Logout from './components/Logout';
+import Profile from './components/Profile';
+import ShowOngoingLot from './components/ShowOngoingLot';
+import Gallery from './components/Gallery';
+import { ToastContainer } from 'react-toastify';
+import Logistics from './components/Logistics';
+import Update from './components/Update';
+import SeefOrders from './components/SeefOrders';
+import Seemaxbids from './components/Seemaxbids';
+import Finalorders from './components/Finalorders';
+import Seevorders from './components/Seevorders';
+import Finalbids from './components/Finalbids';
+import Seefeedback from './components/Seefeedback';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+const farmerStore = createStore(FarmerReducer, composeWithDevTools());
+
+const record = (
+  <>
+    <Provider store={farmerStore}>
+      <BrowserRouter>
+        <ToastContainer />
+        <Header />
+        <Routes>
+          <Route path='/Seefeedback' element={<Seefeedback />} />
+          <Route path='/Seemaxbids/:fpid' element={<Seemaxbids />} />
+          <Route path='/Finalorders/:fid' element={<Finalorders />} />
+          <Route path='/Seevorders' element={<Seevorders />} />
+          <Route path='/Finalbids/:vendorid' element={<Finalbids />} />
+          <Route path='/SeefOrders' element={<SeefOrders />} />
+          <Route path='/Logistics' element={<Logistics />} />
+          <Route path='/Update' element={<Update />} />
+          <Route path='/Gallery' element={<Gallery />} />
+          <Route path='/ShowOngoingLot' element={<ShowOngoingLot />} />
+          <Route path='/Profile' element={<Profile />} />
+          <Route path='/Logout' element={<Logout />} />
+          <Route path='/Vendordashboard' element={<Vendordashboard />} />
+          <Route path='/VendorLogin' element={<VendorLogin />} />
+          <Route path='/Vendor' element={<Vendor />} />
+          <Route path='/TodaysRates' element={<TodaysRates />} />
+          <Route path='/AddProducts' element={<AddProducts />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/homepage' element={<Home />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/Registration' element={<Registration />} />
+          <Route path='/Contactus' element={<Contactus />} />
+          <Route path='/AboutUs' element={<AboutUs />} />
+          <Route path='/Pagenotfound' element={<Pagenotfound />} />
+          <Route path='/farmerlogin' element={<FarmerLogin />} />
+          <Route path='/forgotpass' element={<ForgotPassword />} />
+          <Route path='/Farmer' element={<Farmer />} />
+          <Route path='/Farmerdashboard' element={<Farmerdashboard />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </Provider>
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(record);
